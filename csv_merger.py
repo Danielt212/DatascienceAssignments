e068b4e4b03809fb3bb074655824afd78ef240c3
Danielt212 <160595815+Danielt212@users.noreply.github.com>
2025-03-08 11:19:07 +0000

    1nd commit

--- csv_merger.py
+++ csv_merger.py
@@ -5,7 +5,7 @@ master = []
 
 for file in os.listdir(os.getcwd()):
     if file.startswith('sales_'):
-        master.append(pd.read_csv(file))  
+        master.append(pd.read_csv(file,on_bad_lines='skip'))  
 
 master_df = pd.concat(master, ignore_index=True)
 master_df.to_csv('all_sales.csv', index=False)
