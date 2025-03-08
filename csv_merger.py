7bf681dd4bd89eb28973db9cd644466bdab676f2
Danielt212 <160595815+Danielt212@users.noreply.github.com>
2025-03-08 11:52:40 +0000

    March8 Morning Session

--- csv_merger.py
+++ csv_merger.py
@@ -1,11 +1,22 @@
 import os
 import pandas as pd
 
-master = []
+master_sales = []
+master_crashes = []
+master_ratings = []
 
 for file in os.listdir(os.getcwd()):
     if file.startswith('sales_'):
-        master.append(pd.read_csv(file,on_bad_lines='skip'))  
+        master_sales.append(pd.read_csv(file,on_bad_lines='skip'))  
+    elif file.startswith('stats_crashes_') and file.endswith('_overview.csv'):
+        master_crashes.append(pd.read_csv(file, on_bad_lines='skip'))
+    elif file.startswith('stats_ratings_') and file.endswith('_overview.csv'):
+        master_ratings.append(pd.read_csv(file, on_bad_lines='skip'))
 
-master_df = pd.concat(master, ignore_index=True)
-master_df.to_csv('all_sales.csv', index=False)
+crashes_df = pd.concat(master_crashes, ignore_index=True)
+ratings_df = pd.concat(master_ratings, ignore_index=True)
+sales_df = pd.concat(master_sales, ignore_index=True)
+
+crashes_df.to_csv('all_crashes.csv', index=False)
+ratings_df.to_csv('all_ratings.csv', index=False)
+sales_df.to_csv('all_sales.csv', index=False)
